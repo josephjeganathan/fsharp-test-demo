abc647bd0135d3db3c769ea1f00144f91d038a47
josephjeganathan <josephjeganathan@gmail.com>
2017-07-12 23:43:29 +1000

    FsUnit

--- Calc.FShap.Tests/CalculatorTests.fs
+++ Calc.FShap.Tests/CalculatorTests.fs
@@ -2,35 +2,29 @@
 
 open Xunit
 open Calc
+open FsUnit
+open FsUnit.Xunit
 
 [<Fact>]
 let ``When two positive numbers 1 and 2 added then 3 must be returned``() =
 
-    let actual = Calculator.Add(1, 2)
-
-    Assert.Equal(3, actual)
+    Calculator.Add(1, 2) |> should equal 3
 
 [<Fact>]
 let ``When two negative numbers -1 and - 2 added then -3 must be returned``() =
 
-    let actual = Calculator.Add(-1, -2)
-
-    Assert.Equal(-3, actual)
+    Calculator.Add(-1, -2) |> should equal -3
 
 [<Fact>]
 let ``When 1 and -2 is added then -1 must be returned``() =
 
-    let actual = Calculator.Add(1, -2)
-
-    Assert.Equal(-1, actual)
+    Calculator.Add(1, -2) |> should equal -1
 
 [<Theory>]
 [<InlineData(1, 2, 3)>]
 [<InlineData(-1, -2, -3)>]
 [<InlineData(1, -2, -1)>]
 [<InlineData(100, 200, 300)>]
-let ``When two number x and y added then an expected number must be retuned``(x, y, expected) =
+let ``When two number x and y added then an expected number must be retuned``(x, y, expectedValue) =
 
-    let actual = Calculator.Add(x, y)
-
-    Assert.Equal(expected, actual)
+    Calculator.Add(x, y) |> should equal expectedValue
