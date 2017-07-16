7ba1e67631cc2aad73a387360af8d79d506a408e
josephjeganathan <josephjeganathan@gmail.com>
2017-07-16 14:34:03 +1000

    FsCheck

---
+++ Calc.Fsharp.FsCheck.Tests/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Calc.Fsharp.FsCheck.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Calc.Fsharp.FsCheck.Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Calc.Fsharp.FsCheck.Tests")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("9dfc72e1-5cfa-4e2d-8320-73803c7a6926")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Calc.Fsharp.FsCheck.Tests/CalculatorTests.fs
@@ -0,0 +1,45 @@
+﻿module ``Calculator Tests``
+
+open Xunit
+open Calc
+open FsCheck
+
+let add x y =
+    let calculator = new Calculator()
+    calculator.Add(x, y)
+    calculator.Result
+
+let equal (expected:int) (actual:int) =
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``When two positive numbers 1 and 2 added then 3 must be returned``() =
+
+    add 1 2 |> equal 3
+
+[<Fact>]
+let ``When two negative numbers -1 and - 2 added then -3 must be returned``() =
+
+    add -1 -2 |> equal -3
+
+[<Fact>]
+let ``When 1 and -2 is added then -1 must be returned``() =
+
+    add 1 -2 |> equal -1
+
+[<Theory>]
+[<InlineData(1, 2, 3)>]
+[<InlineData(-1, -2, -3)>]
+[<InlineData(1, -2, -1)>]
+[<InlineData(100, 200, 300)>]
+let ``When two number x and y added then an expected number must be retuned``(x, y, expectedValue) =
+
+    add x y |> equal expectedValue
+
+[<Fact>]
+let ``When two number added then an expected number must be retuned``() =
+
+    let ``Add x, y must be x + y`` x y =
+        add x y = x + y
+
+    Check.QuickThrowOnFailure ``Add x, y must be x + y``
