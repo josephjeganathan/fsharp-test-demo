461d08bec8ad0e8e70cc84ca0df216f8599fc7e6
josephjeganathan <josephjeganathan@gmail.com>
2017-07-20 20:30:14 +1000

    Tickspec & NUnit

---
+++ Calc.Fsharp.TickSpec.Tests/CalcStepDefinitions.fs
@@ -0,0 +1,18 @@
+﻿module StockStepDefinitions
+
+open TickSpec
+open NUnit.Framework
+open Calc
+
+type CalcFeature () = inherit FeatureFixture("Calc.txt")
+
+let mutable calc = Calculator()
+
+let [<Given>] ``a calculator`` () = 
+    calc <- Calculator()
+
+let [<When>] ``(.*) and (.*) is added``(x, y) = 
+    calc.Add(x, y)
+
+let [<Then>] ``result must be (.*)`` (expectedResult) =
+    Assert.AreEqual(expectedResult, calc.Result)
